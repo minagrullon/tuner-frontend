254c4d27b22a9db01131aaecdf6dd3f12bcd53ea
minagrullon <minagrullon@pursuit.org>
2023-01-20 14:05:29 -0500

    app.js routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+//dependencies
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//pages
+import Home from "./Pages/Home";
+import Edit from "./Pages/Edit";
+import New from "./Pages/New";
+import Index from "./Pages/Index";
+import Show from "./Pages/Show";
+
+//components
+import Navbar from "./Components/Navbar";
+
 function App() {
   return (
     <div className="">
       <h1>Tuner App</h1>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/songs" element={<Index />} />
+            <Route path="/songs/new" element={<New />} />
+            <Route path="/songs/:id" element={<Show />} />
+            <Route path="/songs/:id/edit" element={<Edit />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
